906e2c851fdae3066b5770ca4ae613bf4d09e6c5
al-sami <ali_sami988@outlook.com>
2021-01-04 00:13:01 +0300

    Image-Shadow

--- resources/admin/pages/Test.vue
+++ resources/admin/pages/Test.vue
@@ -1,12 +1,17 @@
 <template>
-  <vue-flip :active-hover="true" width="200px" height="50px">
-  <template v-slot:front>
-    front
-  </template>
-  <template v-slot:back>
-    back
-  </template>
-</vue-flip>
+  <!-- <vue-flip active-click="" width="200px" height="50px">
+          <template v-slot:front class="front">
+            front
+          </template>
+          <template v-slot:back class="back">
+            back
+          </template>
+        </vue-flip> -->
+      <div class="demo">
+    <image-shadow :width="280" :src="this.src1" shadow-hover></image-shadow>
+    <image-shadow :width="280" :src="this.src2" shadow-hover></image-shadow>
+    <image-shadow :width="280" :shadow-radius="20" :src="this.src3" shadow-hover></image-shadow>
+  </div>
 
 <!-- <vue-flip active-click></vue-flip>
 <vue-flip active-hover></vue-flip>
@@ -18,11 +23,21 @@
 <script>
 import VueFlip from 'vue-flip';
 import Flipbook from 'flipbook-vue'
+import ImageShadow from 'vue-image-shadow'
+
 
 export default {
   components: {
      VueFlip,
-     Flipbook
+     Flipbook,
+     ImageShadow
+  },
+  data() {
+    return {
+      src1: 'https://github.com/image-component/gallery/blob/main/girl/4.jpg?raw=true',
+      src2: 'https://github.com/image-component/gallery/blob/main/girl/6.jpg?raw=true',
+      src3: 'https://github.com/image-component/gallery/blob/main/girl/8.png?raw=true'
+    }
   }
 }
 </script>
