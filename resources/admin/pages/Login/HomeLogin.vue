b69c993e9279a8694471196ff6d02c901f013c64
al-sami <ali_sami988@outlook.com>
2021-01-05 23:36:22 +0300

    4 Login Forms

--- resources/admin/pages/Login/HomeLogin.vue
+++ resources/admin/pages/Login/HomeLogin.vue
@@ -1,5 +1,8 @@
 <template>
   <b-container>
+    <div class=" col-md-12">
+      <flipbook class="flipbook" :pages="this.pages"></flipbook>
+    </div>
     <b-col md="3" class="py-3"></b-col>
       <b-col md="3" class="py-3"></b-col>
         <div class="row">
@@ -17,7 +20,7 @@
                           </vue-flip>
                         </div> -->
                         <div class="col-12">
-                          <vue-flip active-hover width="400px" height="200px">
+                          <vue-flip active-hover width="400px" height="200px" transition="1.5s">
                               <template v-slot:front class="front">
                                 <image-shadow :width="400" :src="src1"></image-shadow>
                               </template>
@@ -35,7 +38,7 @@
                 <div class="card card-stats" @click="AgentLogin()">
                   <div class="card-body">
                         <div class="col-12">
-                          <vue-flip active-hover width="400px" height="200px">
+                          <vue-flip active-hover width="400px" height="200px" transition="1.5s">
                               <template v-slot:front class="front">
                                 <image-shadow :width="400" :src="src2"></image-shadow>
                               </template>
@@ -51,12 +54,19 @@
        
         <br>
         <br>
-        <!-- <div class="row">
+        <div class="row">
            <div class=" col-md-5">
                 <div class="card card-stats" @click="TayarLogin()">
                   <div class="card-body">
-                        <div class="col-12">
-                              <image-shadow :width="400" :shadow-radius="20" :src="this.src3" shadow-hover></image-shadow>
+                         <div class="col-12">
+                          <vue-flip active-hover width="400px" height="200px" transition="1.5s">
+                              <template v-slot:front class="front">
+                                <image-shadow :width="400" :src="src1"></image-shadow>
+                              </template>
+                              <template v-slot:back class="back">
+                                  <image-shadow :width="400" :src="src2"></image-shadow>
+                              </template>
+                            </vue-flip> 
                         </div>
                     </div>
                     <div class="card-footer text-left"><hr><div class="stats"><div><i class="tim-icons icon-sound-wave"></i>Tayar Login</div></div></div></div>
@@ -66,13 +76,20 @@
              <div class=" col-md-5">
                 <div class="card card-stats" @click="LeadLogin()">
                   <div class="card-body">
-                        <div class="col-12">
-                              <image-shadow :width="400" :shadow-radius="20" :src="this.src1" shadow-hover></image-shadow>
-                        </div>
-                    </div>
+                      <div class="col-12">
+                        <vue-flip active-hover width="400px" height="200px" transition="1.5s">
+                            <template v-slot:front class="front">
+                              <image-shadow :width="400" :src="src1"></image-shadow>
+                            </template>
+                            <template v-slot:back class="back">
+                                <image-shadow :width="400" :src="src2"></image-shadow>
+                            </template>
+                        </vue-flip> 
+                      </div>
+                  </div>
                   <div class="card-footer text-left"><hr><div class="stats"><div><i class="tim-icons icon-sound-wave"></i>Lead Login</div></div></div></div>
             </div>
-        </div> -->
+        </div>
   </b-container>
 </template>
 <script>
@@ -91,17 +108,28 @@ export default {
           src1: 'https://github.com/image-component/gallery/blob/main/girl/4.jpg?raw=true',
           src2: 'https://github.com/image-component/gallery/blob/main/girl/6.jpg?raw=true',
           src3: 'https://github.com/image-component/gallery/blob/main/girl/8.png?raw=true',
+          pages:[
+                'https://github.com/image-component/gallery/blob/main/girl/4.jpg?raw=true',
+                'https://github.com/image-component/gallery/blob/main/girl/6.jpg?raw=true',
+                'https://github.com/image-component/gallery/blob/main/girl/8.png?raw=true',
+                'https://github.com/image-component/gallery/blob/main/girl/4.jpg?raw=true',
+                'https://github.com/image-component/gallery/blob/main/girl/6.jpg?raw=true',
+                'https://github.com/image-component/gallery/blob/main/girl/8.png?raw=true',
+                'https://github.com/image-component/gallery/blob/main/girl/4.jpg?raw=true',
+                'https://github.com/image-component/gallery/blob/main/girl/6.jpg?raw=true',
+                'https://github.com/image-component/gallery/blob/main/girl/8.png?raw=true',
+          ]
       }
     },
     methods:{
         CompanyLogin(){
-          // this.$router.push('/company/login');
+          this.$router.push('/company/login');
         },
         TayarLogin(){
           this.$router.push('/tayar/login');
         },
         AgentLogin(){
-          // this.$router.push('/agent/login');
+          this.$router.push('/agent/login');
         },
         LeadLogin(){
           this.$router.push('/lead/login');
@@ -115,7 +143,9 @@ export default {
 }
 </script>
 <style>
-div.card{
-  margin-top: 50px !important;
+
+.flipbook {
+  width: 90vw;
+  height: 90vh;
 }
 </style>
